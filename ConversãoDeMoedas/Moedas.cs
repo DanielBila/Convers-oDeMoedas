6daba0ad5198accd9547444e2b8a79b92cd86062
danie <danie@DANIEL>
2024-10-17 17:36:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConversãoDeMoedas/Moedas.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversãoDeMoedas
+{
+    internal class Moedas
+    {
+        //Real para outras Moedas
+        public double RealParaDolar(double ValorEmReal)
+        {
+            double dolar = ValorEmReal * 0.18;
+            return dolar;
+        }
+        public double RealParaEuro(double ValorEmReal)
+        {
+            double dolar = ValorEmReal * 0.16;
+            return dolar;
+        }
+       public double RealParaPesos(double ValorEmReal)
+        {
+            double dolar = ValorEmReal * 173.39;
+            return dolar;
+        }
+        //Dolar para outras Moedas
+        public double DolarParaReal(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 5.66;
+            return dolar;
+        }
+        public double DolarParaEuro(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 0.92;
+            return dolar;
+        }
+        public double DolarParaPesos(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 980.75;
+            return dolar;
+        }
+        //Euro para outras Moedas 
+        public double EuroParaReal(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 6.13;
+            return dolar;
+        }
+        public double EuroParaDolar(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 1.08;
+            return dolar;
+        }
+        public double EuroParaPesos(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 1061.94;
+            return dolar;
+        }
+        //Pesso para outras Moedas 
+        public double PesosParaReal(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 0.0058;
+            return dolar;
+        }
+        public double PesosParaDolar(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 0.0010;
+            return dolar;
+        }
+        public double PesosParaEuro(double ValorEmDolar)
+        {
+            double dolar = ValorEmDolar * 0.00094;
+            return dolar;
+        }
+    }
+}
---
+++ ConversãoDeMoedas/Program.cs
@@ -0,0 +1,204 @@
+﻿using ConversãoDeMoedas;
+
+namespace ConversãoDeMoedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Moedas moeda = new Moedas();
+            Console.WriteLine("Escolha a moeda Nativa para ser convertido:");
+            Console.WriteLine("1-Real");
+            Console.WriteLine("2-Dolar");
+            Console.WriteLine("3-Euro");
+            Console.WriteLine("4-Pesos argentinos ");
+            int escolhaDeMoedaNativa = int.Parse(Console.ReadLine());
+            Console.Clear();
+            //Moeda Nativa Real
+            if(escolhaDeMoedaNativa == 1)
+            {
+                Console.WriteLine("Qual Seria a Moeda que sera convertida:");     
+                Console.WriteLine("1-Dolar");
+                Console.WriteLine("2-Euro");
+                Console.WriteLine("3-Pesos argentinos ");
+                int escolhaDeMoedaDeConversao = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if(escolhaDeMoedaDeConversao == 1)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Real para ser convertido em Dolar:");
+                    double valorNatural  = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.RealParaDolar(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Real:{valorNatural}");
+                    Console.WriteLine($"Em Dolar: {valorConvertido}");                
+                }
+                //Real para Euro
+                else if (escolhaDeMoedaDeConversao == 2)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Real para ser convertido em Euro:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.RealParaEuro(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Real:{valorNatural}");
+                    Console.WriteLine($"Em Euro: {valorConvertido}");
+                }
+                //Real para Pesos argentinos
+                else if (escolhaDeMoedaDeConversao == 3)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Real para ser convertido em Pesos argentinos:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.RealParaPesos(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Real:{valorNatural}");
+                    Console.WriteLine($"Em Pesos argentinos: {valorConvertido}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                    Task.Delay(1000).Wait();
+                    Main(args);
+                    Console.Clear();
+                }
+            }
+            //Moeda Nativa Dolar 
+            else if (escolhaDeMoedaNativa == 2)
+            {
+                Console.WriteLine("Qual Seria a Moeda que sera convertida:");
+                Console.WriteLine("1-Real");
+                Console.WriteLine("2-Euro");
+                Console.WriteLine("3-Pesos argentinos ");
+                int escolhaDeMoedaDeConversao = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (escolhaDeMoedaDeConversao == 1)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Dolar para ser convertido em Real:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.DolarParaReal(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Dolar:{valorNatural}");
+                    Console.WriteLine($"Em Real: {valorConvertido}");
+                }
+                else if (escolhaDeMoedaDeConversao == 2)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Dolar para ser convertido em Euro:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.DolarParaEuro(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Dolar:{valorNatural}");
+                    Console.WriteLine($"Em Euro: {valorConvertido}");
+                }
+                else if (escolhaDeMoedaDeConversao == 3)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Dolar para ser convertido em Pesos argentinos:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.DolarParaPesos(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Dolar:{valorNatural}");
+                    Console.WriteLine($"Em Pesos argentinos: {valorConvertido}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                    Task.Delay(1000).Wait();
+                    Main(args);
+                    Console.Clear();
+                }
+            }
+            //Dolar Nativa Euro
+            else if (escolhaDeMoedaNativa == 3)
+            {
+                Console.WriteLine("Qual Seria a Moeda que sera convertida:");
+                Console.WriteLine("1-Real");
+                Console.WriteLine("2-Dolar");
+                Console.WriteLine("3-Pesos argentinos ");
+                int escolhaDeMoedaDeConversao = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (escolhaDeMoedaDeConversao == 1)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Euro para ser convertido em Real:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.EuroParaReal(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Euro:{valorNatural}");
+                    Console.WriteLine($"Em Real: {valorConvertido}");
+                }
+                else if (escolhaDeMoedaDeConversao == 2)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Euro para ser convertido em Dolar:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.EuroParaDolar(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Euro:{valorNatural}");
+                    Console.WriteLine($"Em Dolar: {valorConvertido}");
+                }
+                else if (escolhaDeMoedaDeConversao == 3)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Euro para ser convertido em Pesos argentinos:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.EuroParaPesos(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Euro:{valorNatural}");
+                    Console.WriteLine($"Em Pesos argentinos: {valorConvertido}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                    Task.Delay(1000).Wait();
+                    Main(args);
+                    Console.Clear();
+                }
+            }
+            //Pesso para outras moedas 
+            else if (escolhaDeMoedaNativa == 4)
+            {
+                Console.WriteLine("Qual Seria a Moeda que sera convertida:");
+                Console.WriteLine("1-Real");
+                Console.WriteLine("2-Dolar");
+                Console.WriteLine("3-Euro");
+                int escolhaDeMoedaDeConversao = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (escolhaDeMoedaDeConversao == 1)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Pesos argentinos  para ser convertido em Real:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.PesosParaReal(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Pesos argentinos:{valorNatural}");
+                    Console.WriteLine($"Em Real: {valorConvertido}");
+                }
+                else if (escolhaDeMoedaDeConversao == 2)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Pesos argentinos para ser convertido em Dolar:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.PesosParaDolar(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Pesos argentinos:{valorNatural}");
+                    Console.WriteLine($"Em Dolar: {valorConvertido}");
+                }
+                else if (escolhaDeMoedaDeConversao == 3)
+                {
+                    Console.WriteLine("Qual Seria o Valor em Pesos argentinos para ser convertido em Euro:");
+                    double valorNatural = double.Parse(Console.ReadLine());
+                    double valorConvertido = moeda.PesosParaEuro(valorNatural);
+                    Console.Clear();
+                    Console.WriteLine($"Pesos argentinos:{valorNatural}");
+                    Console.WriteLine($"Em Euro: {valorConvertido}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro");
+                    Task.Delay(1000).Wait();
+                    Main(args);
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Erro");
+                Task.Delay(1000).Wait();
+                Main(args);
+                Console.Clear();
+            }
+        }
+    }
+}
